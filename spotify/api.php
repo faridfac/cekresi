1eef97817bc546c83530b7126fd76adaa361ece2
Muhamad Farid <muhammadfarid.alfachri@gmail.com>
2020-04-17 19:50:16 +0700

    Add files via upload

---
+++ spotify/api.php
@@ -0,0 +1,29 @@
+<?php
+require "function.php";
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET');
+$maintenance = "0";
+if ($maintenance == "1") {
+  $result = array('result' => false, 'data' => null, 'message' => 'Maintenance');
+	exit(json_encode($result));
+}
+
+if ($_GET) {
+  $spotify = new spotify();
+  $nama = $spotify->nama();
+  $pecah = explode(" ",$nama);
+  $email = strtolower($pecah[0].$pecah[1].rand(10,9999))."@gmail.com";
+  $pass = $_GET['pass'];
+  $create = $spotify->createAccount($email, $nama, $pass);
+  $js = json_decode($create, true);
+  if ($js['status'] == "1") {
+    $result = array('result' => true, 'data' => array('email' => ''.$email.'', 'pass' => ''.$pass.''), 'message' => 'Successfully Register');
+  } else {
+    $err = $spotify->get_between($create, 'errors":{"', '"},"country');
+    $result = array('result' => false, 'data' => null, 'message' => ''.$err.'');
+  }
+} else {
+    $result = array('result' => false, 'data' => null, 'message' => 'Permintaan tidak sesuai');
+}
+print json_encode($result, JSON_PRETTY_PRINT);
+?>

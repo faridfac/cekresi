e630db88377d7861ecd4d8f552b83800493efb7c
Muhamad Farid <muhammadfarid.alfachri@gmail.com>
2021-09-13 23:22:07 +0700

    Update header.php

--- lib/header.php
+++ lib/header.php
@@ -20,7 +20,7 @@ if ($config['web']['maintenance'] == 1) {
 <script src="assets/js/main.js" type="text/javascript"></script>
 <script src="assets/js/bootstrap.min.js" type="text/javascript"></script>
 <style type="text/css">.hidden{display:none}.ui-autocomplete-loading{background:#fff url(assets/img/ui-anim_basic_16x16.gif) right center no-repeat}.ui-menu-item{font-weight:700;font-size:11px}.table-result{font-size:11px}.table-result th{padding:5px 3px!important}.table-result td{padding:5px 3px!important}.services{background:#d7e8fa;padding:30px 0;min-height:200px;color:#333;border-bottom:solid 1px #ccc}.about{background:#d7e8fa;padding:60px 0;min-height:500px;color:#333;border-bottom:solid 1px #ccc}.footer-bg{color:#fff;background:#1e1e1e;padding:60px 0;font-size:11px}.top_title{text-align:center;margin-bottom:15px}.site-footer{margin:40px 0 0;padding:10px 0 10px;border-top:1px solid #ddd;color:#999;background:#fff;font-size:13px;line-height:1.55em;font-family:Source Sans Pro;text-align:center}</style>
-<script src="assets/js/debug-protection.js"></script>
+//<script src="assets/js/debug-protection.js"></script>
 <script>if(self != top) { top.location = self.location; }</script>
 </head>
 <body>
